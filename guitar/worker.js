3a09f74bdcfc57edb8466fb093dc922d2bf7d00e
Phil Willoughby <willerz@gmail.com>
2026-02-01 15:02:31 +0000

    Prep for more tools, second neck tool, analytics

--- guitar/worker.js
+++ guitar/worker.js
@@ -28,78 +28,43 @@ async function load3mf(url) {
     return blob.arrayBuffer();
 }
 
-function prepareCad([cad, body, pickguard, controlCover, neckPocket, neckPickup, bridgePickup, bridgeTool, bodyScript, pickguardScript, controlCoverScript]) {
-    cad.FS.writeFile("/body.3mf", new Int8Array(body));
-    cad.FS.writeFile("/pickguard.3mf", new Int8Array(pickguard));
-    cad.FS.writeFile("/controlCover.3mf", new Int8Array(controlCover));
+function prepareCad(cad, body, tools) {
+    cad.FS.writeFile("/body.3MF", new Int8Array(body));
 
-    cad.FS.writeFile("/neckPocket.3mf", new Int8Array(neckPocket));
-    cad.FS.writeFile("/neckPickup.3mf", new Int8Array(neckPickup));
-    cad.FS.writeFile("/bridgePickup.3mf", new Int8Array(bridgePickup));
-    cad.FS.writeFile("/bridgeTool.3mf", new Int8Array(bridgeTool));
+    const script = [
+        "difference () {",
+        '    import("body.3MF");',
+    ];
 
-    cad.FS.writeFile("/bodyScript.scad", new Int8Array(bodyScript));
-    cad.FS.writeFile("/pickguardScript.scad", new Int8Array(pickguardScript));
-    cad.FS.writeFile("/controlCoverScript.scad", new Int8Array(controlCoverScript));
+    tools.map((tool, index)=>{
+        script.push(`    import("tool${index}.3MF");`);
+        cad.FS.writeFile(`/tool${index}.3MF`, new Int8Array(tool));
+    });
+    script.push('}');
+
+    cad.FS.writeFile("/script.scad", script.join("\n"));
     return cad;
 }
 
 async function computeGuitar(msg) {
-    const bodyReq = load3mf("body/batlike.3MF");
-    const pickguardReq = load3mf("body/batlike-guard.3MF");
-    const controlCoverReq = load3mf("body/batlike-cover.3MF");
-
-    const neckPocketToolReq = load3mf("tool/neck/strat.3MF");
-    const neckPickupToolReq = load3mf("tool/pickup/neck/humbucker.3MF");
-    const bridgePickupToolReq = load3mf("tool/pickup/bridge/humbucker.3MF");
-    const bridgeToolReq = load3mf("tool/bridge/musiclily-ultra.3MF");
-
-    const bodyScriptReq = load3mf("scripts/body.scad");
-    const pickguardScriptReq = load3mf("scripts/pickguard.scad");
-    const controlCoverScriptReq = load3mf("scripts/control-cover.scad");
-
-    const bodyCadReq = await OpenSCAD({noInitialRun: true});
-    const pickguardCadReq = await OpenSCAD({noInitialRun: true});
-    const controlCoverCadReq = await OpenSCAD({noInitialRun: true});
-
-    const [bodyCad, pickguardCad, controlCoverCad, body, pickguard, controlCover, neckPocket, neckPickup, bridgePickup, bridgeTool, bodyScript, pickguardScript, controlCoverScript] = await Promise.all([
-        bodyCadReq,
-        pickguardCadReq,
-        controlCoverCadReq,
-        bodyReq,
-        pickguardReq,
-        controlCoverReq,
-        neckPocketToolReq,
-        neckPickupToolReq,
-        bridgePickupToolReq,
-        bridgeToolReq,
-        bodyScriptReq,
-        pickguardScriptReq,
-        controlCoverScriptReq,
-    ]);
-    
     try {
-        prepareCad([bodyCad, body, pickguard, controlCover, neckPocket, neckPickup, bridgePickup, bridgeTool, bodyScript, pickguardScript, controlCoverScript]);
-        prepareCad([pickguardCad, body, pickguard, controlCover, neckPocket, neckPickup, bridgePickup, bridgeTool, bodyScript, pickguardScript, controlCoverScript]);
-        prepareCad([controlCoverCad, body, pickguard, controlCover, neckPocket, neckPickup, bridgePickup, bridgeTool, bodyScript, pickguardScript, controlCoverScript]);
-        
-        bodyCad.callMain(["/bodyScript.scad", "-o", "/bodyOut.stl"]);
-        const bodyModel = bodyCad.FS.readFile("/bodyOut.stl");
-        
-        pickguardCad.callMain(["/pickguardScript.scad", "-o", "pickguard-out.stl"]);
-        const pickguardModel = pickguardCad.FS.readFile("/pickguard-out.stl");
-        
-        controlCoverCad.callMain(["/controlCoverScript.scad", "-o", "controlCover-out.stl"]);
-        const controlCoverModel = controlCoverCad.FS.readFile("/controlCover-out.stl");
+        const bodyReqs = msg.data.bodies.map(load3mf);
+        const toolReqs = msg.data.tools.map(load3mf);
+        const cadReqs = msg.data.bodies.map(() => OpenSCAD({noInitialRun: true}));
 
-        const bodyModelBlob = new Blob([bodyModel], { type: "model/stl" });
-        const pickguardModelBlob = new Blob([pickguardModel], { type: "model/stl" });
-        const controlCoverModelBlob = new Blob([controlCoverModel], { type: "model/stl" });
-        postMessage(
-        '<a download="body.stl" href="' + URL.createObjectURL(bodyModelBlob) + '">Download the body model</a>, ' + 
-        '<a download="pickguard.stl" href="' + URL.createObjectURL(pickguardModelBlob) + '">Download the pickguard model</a>, and ' +
-        '<a download="controlCover.stl" href="' + URL.createObjectURL(controlCoverModelBlob) + '">Download the control cover model</a>' +
-        '');
+        const bodies = await Promise.all(bodyReqs);
+        const tools = await Promise.all(toolReqs);
+        const cads = await Promise.all(cadReqs);
+
+        const links = [];
+
+        cads.map((cad, index) => {
+            prepareCad(cad, bodies[index], tools);
+            cad.callMain(["/script.scad", "-o", "/output.stl"]);
+            const blob = new Blob([cad.FS.readFile("/output.stl")], { type: "model/stl" });
+            links.push(`<a download="part${index + 1}.stl" href="${URL.createObjectURL(blob)}">Download STL for part ${index + 1}</a>`);
+        });
+        postMessage(links);
     }
     catch(err) {
         postMessage("failed");
